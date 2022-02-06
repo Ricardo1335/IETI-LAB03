8444f5283f0b592f4856e48b70ae62d79ba75db1
Ricardo Amaya <ricardo.amaya@escuelaing.edu.co>
2022-02-06 18:09:06 -0500

    Fixed

--- src/main/java/edu/eci/IETILAB021/repository/UserServiceMongoDB.java
+++ src/main/java/edu/eci/IETILAB021/repository/UserServiceMongoDB.java
@@ -13,45 +13,45 @@ public class UserServiceMongoDB
    implements UserService
    {
    
-       private final UserRepository userRepository;
-   
-       public UserServiceMongoDB(@Autowired UserRepository userRepository )
-       {
-           this.userRepository = userRepository;
-       }
-   
-       @Override
-        public User create(User user) {
-            return userRepository.save(user); 
-        }
+    private final UserRepository userRepository;
 
-        @Override
-        public User findById(String id) {
-            if(userRepository.existsById(id))return userRepository.findById(id).get();
-            return null; 
-        }
+    public UserServiceMongoDB(@Autowired UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        @Override
-        public boolean deleteById(String id) {
-            userRepository.deleteById(id);
-            return true;
-        }
+    @Override
+    public User create(User user) {
+        System.out.print("entra"); 
+        return userRepository.save(user); 
+    }
 
-        @Override
-        public List<User> getAll() {
-            return userRepository.findAll(); 
-        }
+    @Override
+    public User findById(String id) {
+        if(userRepository.existsById(id))return userRepository.findById(id).get();
+        return null; 
+    }
 
-        @Override
-        public User update(User user, String userId) {
-            if(userRepository.existsById(userId)){
-                User actualUser = userRepository.findById(userId).get(); 
-                actualUser.setCreatedAt(user.getCreateAt());
-                actualUser.setEmail(user.getEmail());
-                actualUser.setLastname(user.getLastName());
-                actualUser.setName(user.getName());
-                return actualUser; 
-            }    
-            return null; 
+    @Override
+    public void deleteById(String id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll(); 
+    }
+
+    @Override
+    public User update(User user, String userId) {
+        if(userRepository.existsById(userId)){
+            User actualUser = userRepository.findById(userId).get(); 
+            actualUser.setCreatedAt(user.getCreatedAt());
+            actualUser.setEmail(user.getEmail());
+            actualUser.setLastName(user.getLastName());
+            actualUser.setName(user.getName());
+            userRepository.save(actualUser); 
+            return actualUser; 
+        } 
+        return null; 
     }
    }
